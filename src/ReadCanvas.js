b57e99275cd593ace09e35200ea4d560e045e569
d.shaklin <d.shaklin@telecontact.ru>
2020-04-14 14:11:19 +0300

    -Add pdf reader -Starting realizate swiping

--- src/ReadCanvas.js
+++ src/ReadCanvas.js
@@ -1,38 +1,74 @@
 import React from 'react';
-import kkk from './lol.txt'
+import PDFJS from 'pdfjs-dist'
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
+PDFJS.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
 
 class ReadCanvas extends React.Component {
 
-    constructor(props) {
+    constructor(props) { 
         super(props);
-        this.state = {text: ''};
+        this.state = {text: '',
+                      pdf: new Object(),
+                      swipedir: '',
+                      curPage:0};
+
+        var startX,
+            startY,
+            dist,
+            distX,
+            distY,
+            threshold = 150, //required min distance traveled to be considered swipe
+            restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+            allowedTime = 300, // maximum time allowed to travel that distance
+            elapsedTime,
+            startTime;
+
+        document.addEventListener('touchstart', (e) => {
+            this.swipedir = 'none'
+            dist = 0
+            startX = e.touches[0].clientX
+            startY = e.touches[0].clientY
+            startTime = new Date().getTime() // record time when finger first makes contact with surface
+        });
+
+        document.addEventListener('touchend', (e) => {
+            var touchobj = e.changedTouches[0]
+
+            if(touchobj == undefined){
+                return
+            }
+
+            distX = touchobj.clientX - startX // get horizontal dist traveled by finger while in contact with surface
+            distY = touchobj.clientY - startY // get vertical dist traveled by finger while in contact with surface
+            elapsedTime = new Date().getTime() - startTime // get time elapsed
+            if (elapsedTime <= allowedTime){ // first condition for awipe met
+                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                    this.swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+                }
+                else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                    this.swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+                }
+                console.log(this.swipedir);
+            }
+
+            switch (this.swipedir) {
+                case 'left':
+                    this.curPage--;
+                    break;
+                case 'right':
+                    this.curPage++;;
+                    break;
+
+                default:
+            }
+
+            console.log(this.curPage);
+
+        }, false)
+
     }
 
-
-    wrapText(context, text, x, y, maxWidth, lineHeight) {
-        var words = text.split(' ');
-        var line = '';
-
-        for(var n = 0; n < words.length; n++) {
-            var testLine = line + words[n] + ' ';
-            var metrics = context.measureText(testLine);
-            var testWidth = metrics.width;
-            if (testWidth > maxWidth && n > 0) {
-                console.log(line);
-                context.fillText(line, x, y);
-                line = words[n] + ' ';
-                y += lineHeight;
-            }
-            else {
-                line = testLine;
-            }
-        }
-
-        console.log('wrap text');
-        console.log(line);
-
-        context.fillText(line, x, y);
-    }
 
     componentDidMount() {
         var reader = new FileReader();
@@ -41,45 +77,36 @@ class ReadCanvas extends React.Component {
             var text = reader.result;
         }
 
-        fetch('http://localhost:3004')
-            .then(res => {
-                console.log(res);
-                return res.text();
-            })
-            .then(res => {
-                console.log(res);
-                this.setState({text: res})
-                console.log('set text');
-                this.updateCanvas();
-            });
-    }
-    updateCanvas() {
-        this.draw()
+        PDFJS.getDocument('http://localhost:3004').then(
+            (pdfFile)=>{
+                this.setState({pdf:pdfFile});
+            }
+        );
+
     }
 
-    draw() {
-        console.log('draw func');
-
-        var ctx = document.getElementById('canvas').getContext('2d');
-        ctx.font = "12px serif";
-        if(this.state.text !== null){
+    openPage(number){
+        this.state.pdf.getPage(number).then(function(page) {
+            var scale = 1;
+            var viewport = page.getViewport({ scale: scale, });
 
             var canvas = document.getElementById('canvas');
             var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
 
-            var maxWidth = 400;
-            var lineHeight = 24;
-            var x = (canvas.width - maxWidth) / 2;
-            var y = 60;
-            var text = this.state.text;
-            context.fillStyle = '#333';
-            this.wrapText(context, text, x, y, maxWidth, lineHeight);
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
 
-        }
+            page.render(renderContext);
+            // you can now use *page* here
+        });
     }
 
     render() {
-        return <canvas id={'canvas'} width={640} height={425} />;
+        return <canvas id={'canvas'} width={640} height={425}/>;
     }
 }
 
